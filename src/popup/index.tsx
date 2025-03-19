71162e3631eb60d842d3c3393c0474db1a5094e0
RAMASSAMY Luc <luko974@gmail.com>
2025-03-19 10:08:18 +0100

    moved into src, changed nam in manifest

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -11,7 +11,7 @@ function IndexPopup() {
       <h2>
         Welcome to your{" "}
         <a href="https://www.plasmo.com" target="_blank">
-          Plasma
+          Plasmuerhgfoisuhgqs
         </a>{" "}
         Extension!
       </h2>
