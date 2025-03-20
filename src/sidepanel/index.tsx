62b5cdb2578ed23adfd8ec25dc9a03b2251a1b4d
Dev-Moulin <p.moulin.95@gmail.com>
2025-03-20 14:26:52 +0100

    add file sprite-loader.ts and utils.ts in lib, file assets, file modify: Navbar.tsx, button.tsx, index.ts, readme.md in /ui from intuition, ThemeProvider.tsx, Home.tsx, index.tsx from /sidepanel, global.css from /src, components.json, package.json, plasmo.config.ts, postcss.config.js, tailwind.config.ts, tsconfig.json from /src

--- src/sidepanel/index.tsx
+++ src/sidepanel/index.tsx
@@ -1,38 +1,44 @@
+import "../global.css"
+
+import { configureClient, createServerClient } from "@0xintuition/graphql"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useState } from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
 import Home from "~src/pages/Home"
 import Profile from "~src/pages/Profile"
+
 import Navbar from "../components/layout/Navbar"
+import { ThemeProvider } from "../components/ThemeProvider"
 import Feed from "../pages/Feed"
-import { configureClient } from '@0xintuition/graphql'
-import { createServerClient } from '@0xintuition/graphql'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const client = createServerClient({
-
-})
+const client = createServerClient({})
 
 configureClient({
   apiUrl: "https://dev.base.intuition-api.com/v1/graphql"
 })
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 function IndexSidepanel() {
   const [data, setData] = useState("")
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Navbar />
-          <Routes>
-          <Route path="*" element={<Home/>} />
-            <Route path="/" element={<Home/>} />
-            <Route path="/profile" element={<Profile/>} />
-            <Route path="/feed" element={<Feed/>} />
-          </Routes>
-      </Router>
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="light" storageKey="intuition-theme">
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Navbar />
+          <div className="container mx-auto p-4">
+            <Routes>
+              <Route path="*" element={<Home />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/feed" element={<Feed />} />
+            </Routes>
+          </div>
+        </Router>
+      </QueryClientProvider>
+    </ThemeProvider>
   )
 }
 
