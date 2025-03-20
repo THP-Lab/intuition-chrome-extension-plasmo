f6a499b74dde387099efd669981422cc4fdd8b56
ZainebPadilla <zainebt16@gmail.com>
2025-03-20 15:46:49 +0100

    add the connect to wallet button

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import { Button } from "@0xintuition/1ui"; 
+
+
 function Profile() {
   return (
-    <h1>Hello from Profile ! </h1>
-  )
-}
+    <div className="flex flex-col h-screen p-4">
+    <h1 className="text-2xl font-bold">Profile Page</h1>
+      <div className="flex flex-col items-center justify-center h-screen p-4 text-center">
+        <Button size="md" variant="accent">Connect to Wallet</Button>
+      </div>
+  </div>
+);
+};
 
 
 export default Profile;
