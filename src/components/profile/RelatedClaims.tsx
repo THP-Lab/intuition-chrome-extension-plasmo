5d56e9588fa4182365cea783ec36661b44f86cc5
Alexe Marichal <marichalalexe@gmail.com>
2025-03-25 13:51:00 +0200

    add address detection via useStorage in all profile tabs

--- src/components/profile/RelatedClaims.tsx
+++ src/components/profile/RelatedClaims.tsx
@@ -1,6 +1,15 @@
+import React from "react";
+import { useStorage } from "@plasmohq/storage/hook";
+
+
 const RelatedClaimsTab = () => {
+  const [address] = useStorage<string>("metamask-account")
+
   return(
-    <h2>This is Identity Tab</h2>
+    <div>
+      <h2>Related Claims</h2>
+      <p>Wallet address : {address || "Not connected"}</p>
+    </div>
   )
 };
 
