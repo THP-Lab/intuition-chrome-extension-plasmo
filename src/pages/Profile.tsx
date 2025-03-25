ad54e38a0505b1f829e05e6e975ac3bd40f59552
Dev-Moulin <p.moulin.95@gmail.com>
2025-03-25 14:29:54 +0100

    fix Background color in pages Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,16 +1,19 @@
-
 import React, { useEffect, useState } from "react"
-import WalletConnectionButton from "~src/components/WalletConnectionButton"
-import ProfileTabs from "~src/components/profile/ProfileTabs"
 import { Outlet } from "react-router-dom"
 import { useStorage } from "@plasmohq/storage/hook";
 
 function Profile() {
-  const [address] = useStorage<string>("metamask-account")
+  const [address, setAddress] = useState(
+    localStorage.getItem("metamask-account")
+  )
+
+  const handleClick = () => {
+    setAddress(localStorage.getItem("metamask-account"))
+  }
 
   if (!address) {
     return (
-      <div>
+      <div className="bg-background">
         Please link your metamask account then re-open this page
         <WalletConnectionButton />
       </div>
@@ -18,12 +21,12 @@ function Profile() {
   }
 
   return (
-    <div>
-      <WalletConnectionButton />
+    <div className="bg-background">
+      <WalletConnectionButton onClick={handleClick} />
       <ProfileTabs />
       <Outlet />
     </div>
   )
 }
 
-export default Profile;
+export default Profile
