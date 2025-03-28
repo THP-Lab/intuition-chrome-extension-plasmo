818b43282b017d8d7fcca6265e0fd4f4c58a1ea4
RAMASSAMY Luc <luko974@gmail.com>
2025-03-28 15:59:25 +0100

    wip: displaying activity of followed users

--- src/graphql/src/generated/index.ts
+++ src/graphql/src/generated/index.ts
@@ -10942,6 +10942,39 @@ export type GetFollowingsFromAddressQuery = {
     image?: string | null
     label: string
     type: any
+    positions_aggregate: {
+      __typename?: "positions_aggregate"
+      nodes: Array<{
+        __typename?: "positions"
+        shares: any
+        vault: {
+          __typename?: "vaults"
+          id: any
+          triple?: {
+            __typename?: "triples"
+            id: any
+            object: {
+              __typename?: "atoms"
+              type: any
+              image?: string | null
+              label?: string | null
+            }
+            predicate: {
+              __typename?: "atoms"
+              type: any
+              image?: string | null
+              label?: string | null
+            }
+            subject: {
+              __typename?: "atoms"
+              type: any
+              image?: string | null
+              label?: string | null
+            }
+          } | null
+        }
+      }>
+    }
   }>
 }
 
@@ -14270,6 +14303,32 @@ export const GetFollowingsFromAddressDocument = `
     image
     label
     type
+    positions_aggregate(limit: 10) {
+      nodes {
+        shares
+        vault {
+          id
+          triple {
+            id
+            object {
+              type
+              image
+              label
+            }
+            predicate {
+              type
+              image
+              label
+            }
+            subject {
+              type
+              image
+              label
+            }
+          }
+        }
+      }
+    }
   }
 }
     `
@@ -23276,7 +23335,161 @@ export const GetFollowingsFromAddress = {
                 { kind: "Field", name: { kind: "Name", value: "id" } },
                 { kind: "Field", name: { kind: "Name", value: "image" } },
                 { kind: "Field", name: { kind: "Name", value: "label" } },
-                { kind: "Field", name: { kind: "Name", value: "type" } }
+                { kind: "Field", name: { kind: "Name", value: "type" } },
+                {
+                  kind: "Field",
+                  name: { kind: "Name", value: "positions_aggregate" },
+                  arguments: [
+                    {
+                      kind: "Argument",
+                      name: { kind: "Name", value: "limit" },
+                      value: { kind: "IntValue", value: "10" }
+                    }
+                  ],
+                  selectionSet: {
+                    kind: "SelectionSet",
+                    selections: [
+                      {
+                        kind: "Field",
+                        name: { kind: "Name", value: "nodes" },
+                        selectionSet: {
+                          kind: "SelectionSet",
+                          selections: [
+                            {
+                              kind: "Field",
+                              name: { kind: "Name", value: "shares" }
+                            },
+                            {
+                              kind: "Field",
+                              name: { kind: "Name", value: "vault" },
+                              selectionSet: {
+                                kind: "SelectionSet",
+                                selections: [
+                                  {
+                                    kind: "Field",
+                                    name: { kind: "Name", value: "id" }
+                                  },
+                                  {
+                                    kind: "Field",
+                                    name: { kind: "Name", value: "triple" },
+                                    selectionSet: {
+                                      kind: "SelectionSet",
+                                      selections: [
+                                        {
+                                          kind: "Field",
+                                          name: { kind: "Name", value: "id" }
+                                        },
+                                        {
+                                          kind: "Field",
+                                          name: {
+                                            kind: "Name",
+                                            value: "object"
+                                          },
+                                          selectionSet: {
+                                            kind: "SelectionSet",
+                                            selections: [
+                                              {
+                                                kind: "Field",
+                                                name: {
+                                                  kind: "Name",
+                                                  value: "type"
+                                                }
+                                              },
+                                              {
+                                                kind: "Field",
+                                                name: {
+                                                  kind: "Name",
+                                                  value: "image"
+                                                }
+                                              },
+                                              {
+                                                kind: "Field",
+                                                name: {
+                                                  kind: "Name",
+                                                  value: "label"
+                                                }
+                                              }
+                                            ]
+                                          }
+                                        },
+                                        {
+                                          kind: "Field",
+                                          name: {
+                                            kind: "Name",
+                                            value: "predicate"
+                                          },
+                                          selectionSet: {
+                                            kind: "SelectionSet",
+                                            selections: [
+                                              {
+                                                kind: "Field",
+                                                name: {
+                                                  kind: "Name",
+                                                  value: "type"
+                                                }
+                                              },
+                                              {
+                                                kind: "Field",
+                                                name: {
+                                                  kind: "Name",
+                                                  value: "image"
+                                                }
+                                              },
+                                              {
+                                                kind: "Field",
+                                                name: {
+                                                  kind: "Name",
+                                                  value: "label"
+                                                }
+                                              }
+                                            ]
+                                          }
+                                        },
+                                        {
+                                          kind: "Field",
+                                          name: {
+                                            kind: "Name",
+                                            value: "subject"
+                                          },
+                                          selectionSet: {
+                                            kind: "SelectionSet",
+                                            selections: [
+                                              {
+                                                kind: "Field",
+                                                name: {
+                                                  kind: "Name",
+                                                  value: "type"
+                                                }
+                                              },
+                                              {
+                                                kind: "Field",
+                                                name: {
+                                                  kind: "Name",
+                                                  value: "image"
+                                                }
+                                              },
+                                              {
+                                                kind: "Field",
+                                                name: {
+                                                  kind: "Name",
+                                                  value: "label"
+                                                }
+                                              }
+                                            ]
+                                          }
+                                        }
+                                      ]
+                                    }
+                                  }
+                                ]
+                              }
+                            }
+                          ]
+                        }
+                      }
+                    ]
+                  }
+                }
               ]
             }
           }
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,10 +1,55 @@
 import React, { useEffect, useState } from "react"
+import ClaimRowLite from "~src/components/ui/ClaimRowLite"
 
+import { useGetFollowingsFromAddressQuery } from "~src/graphql/src"
 
 function Feed() {
-    return (
-      <h1>Page feed</h1>
-    )
+  const accountId = "0x25d5c9dbc1e12163b973261a08739927e4f72ba8"
+  const { data, isLoading, isError } = useGetFollowingsFromAddressQuery({
+    address: accountId
+  })
+
+  const followings = data?.following ?? []
+
+  const default_img = "https://i.seadn.io/gae/PWDq8erM2dMscd99OntjFRJFfvtvki7uxeYiBUT8e59Kdbn8s34dM59kCkVZ66b687B6i8KXMDspRfnU-JbLcB9Kc23EoSydJNkmgA?auto=format&dpr=1&w=1000"
+  if (!accountId) return <p>Connect your wallet</p>
+  if (isLoading) return <p>Loading who you follow...</p>
+  if (isError) return <p>Error loading followings</p>
+
+  return (
+    <div>
+      <h1> Feed page </h1>
+      {followings.map((following) => {
+        const positions = following.positions_aggregate.nodes
+        return (
+          <>
+         <p>{following.label} staked:</p>
+          {positions.map((position) => (
+          <>
+            <p>{position.shares} ETH</p>
+            <p>On :</p>
+            <ClaimRowLite
+              key={position.vault.triple.object.id} 
+              subjectLabel={claim.subject.label ?? "No subject"}
+              subjectImage={claim.subject?.image ?? undefined}
+              predicateLabel={claim.predicate?.label ?? "No predicate"}
+              predicateImage={claim.predicate?.image ?? undefined}
+              objectLabel={claim.object?.label ?? "No object"}
+              objectImage={claim.object?.image ?? undefined}
+              numPositionsFor={claim.vault.positions_aggregate.aggregate?.count ?? 0}
+              numPositionsAgainst={claim.counter_vault.positions_aggregate.aggregate?.count ?? 0}
+              userStake={Number(claim.shares ?? 0)}
+              userCounterStake={Number(claim.counter_shares ?? 0)}
+              isFirst={index === 0}
+              isLast={index === claims.length - 1}
+            />
+          </>
+          ))}
+          </>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Feed
