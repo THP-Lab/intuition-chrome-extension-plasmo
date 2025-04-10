89686d177395c5d93439f42ecafce4fd027edd37
JamesDev292 <james.barthee@orange.fr>
2025-04-10 17:53:34 +0200

    delete handlevote

--- src/components/AtomCard.tsx
+++ src/components/AtomCard.tsx
@@ -39,11 +39,6 @@ interface AtomCardProps {
 export const AtomCard: React.FC<AtomCardProps> = ({ atom }) => {
   const { atomPosition, isVoting, txHash } = useAtomPosition()
 
-  const handleVote = async () => {
-    try {
-      await atomPosition(BigInt(atom.id))
-    } catch (err) {}
-  }
   return (
     <div className="border rounded p-4 my-2">
       <div className="flex items-center mb-2">
@@ -66,7 +61,7 @@ export const AtomCard: React.FC<AtomCardProps> = ({ atom }) => {
         </div>
         <div className="mt-4 flex flex-col items-start">
           <button
-            onClick={handleVote}
+            onClick={atomPosition(BigInt(atom.id))}
             disabled={isVoting}
             className="text-for border border-for rounded-md px-2 py-1 hover:bg-for hover:text-white ml-1">
             ↑
