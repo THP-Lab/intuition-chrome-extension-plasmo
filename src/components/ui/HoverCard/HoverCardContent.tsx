0f1806f806604a22c1582424310b7e61832de9ec
Dev-Moulin <p.moulin.95@gmail.com>
2025-04-14 19:23:20 +0200

    change hovercardcontent

--- src/components/ui/HoverCard/HoverCardContent.tsx
+++ src/components/ui/HoverCard/HoverCardContent.tsx
@@ -19,25 +19,25 @@ export const HoverCardContent = React.forwardRef<HTMLDivElement, HoverCardConten
     return (
       <HoverCardPrimitive.Portal>
         <HoverCardPrimitive.Content
-  ref={ref}
-  className={cn(
-    "hover-card-content",
-    isOpen && "hover-card-content-open",
-    className
-  )}
-  sticky="partial"
-  collisionPadding={16}
-  side="bottom"
-  align="center" 
-  sideOffset={8}
-  avoidCollisions={true}
-  {...domProps}
->
-  {children}
-  <HoverCardPrimitive.Arrow 
-    className="absolute -top-2 left-1/2 -translate-x-1/2 border-8 border-transparent border-b-popover" 
-  />
-</HoverCardPrimitive.Content>
+          ref={ref}
+          className={cn(
+            "hover-card-content",
+            isOpen && "hover-card-content-open",
+            className
+          )}
+          sticky="partial"
+          collisionPadding={16}
+          side="bottom"
+          align="center" 
+          sideOffset={8}
+          avoidCollisions={true}
+          {...domProps}
+        >
+          {children}
+          <HoverCardPrimitive.Arrow 
+            className="absolute -top-2 left-1/2 -translate-x-1/2 border-8 border-transparent border-b-popover" 
+          />
+        </HoverCardPrimitive.Content>
       </HoverCardPrimitive.Portal>
     )
   }
