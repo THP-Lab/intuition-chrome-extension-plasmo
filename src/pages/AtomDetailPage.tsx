a3dfaf23d7318be54c1186377a9422f028aaa70a
Alexe Marichal <marichalalexe@gmail.com>
2025-04-16 11:02:54 +0200

    add route /atoms/:id for atom detail page

---
+++ src/pages/AtomDetailPage.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AtomDetailPage = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Atom details page</h1>
+      <p>Coming soon...</p>
+    </div>
+  )
+}
+
+export default AtomDetailPage;
