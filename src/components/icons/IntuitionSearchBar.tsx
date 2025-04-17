d30801b4e52358444dce0a145e120de96afd973e
James <jamesbarthee@gmail.com>
2025-04-17 10:40:56 +0200

    reduction in onset time search bar + particle/2 + move button disconnect

--- src/components/icons/IntuitionSearchBar.tsx
+++ src/components/icons/IntuitionSearchBar.tsx
@@ -29,7 +29,7 @@ const IntuitionSearchIcon: React.FC<IntuitionSearchIconProps> = ({
       setTimeout(() => {
         wrapper.classList.add("appear")
         setIsAppeared(true)
-      }, 1000)
+      }, 200)
     }
   }, [])
 
@@ -100,7 +100,7 @@ const IntuitionSearchIcon: React.FC<IntuitionSearchIconProps> = ({
             <input
               className="input-bar"
               type="text"
-              placeholder="Rechercher..."
+              placeholder="Search..."
               onChange={handleInputChange}
               onFocus={() =>
                 document
