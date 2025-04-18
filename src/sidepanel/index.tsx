ba919f444b619121e3f6578d316d593a18bc6b3e
Dev-Moulin <p.moulin.95@gmail.com>
2025-04-18 13:13:59 +0200

    feat: implement dual particle system with interactive groups and static background

--- src/sidepanel/index.tsx
+++ src/sidepanel/index.tsx
@@ -1,17 +1,20 @@
 import React from "react"
+
 import Content from "~src/components/content"
+import ParticlesCanvas from "~src/components/ui/ParticulBg/ParticlesCanvas"
+import GroupParticlesCanvas from "~src/components/ui/ParticulBg/GroupParticlesCanvas"
 import { ThemeProvider } from "~src/components/ThemeProvider"
-import ParticlesCanvas from "~src/components/ParticlesCanvas"
 
 function IndexSidepanel() {
   return (
     <>
-    <ThemeProvider defaultTheme="dark" storageKey="intuition-theme">
-      <div className="flex min-h-screen flex-col bg-background text-foreground">
-      <ParticlesCanvas />
-        <Content />
-      </div>
-    </ThemeProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="intuition-theme">
+        <div className="flex min-h-screen flex-col bg-background text-foreground">
+          <ParticlesCanvas />
+          <GroupParticlesCanvas />
+          <Content />
+        </div>
+      </ThemeProvider>
     </>
   )
 }
