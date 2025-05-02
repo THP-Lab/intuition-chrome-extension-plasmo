1a09e6812cd32fd19cbf01b5759d4d0e939ea503
Dev-Moulin <p.moulin.95@gmail.com>
2025-05-02 16:35:22 +0200

    fix import and delet homebanner

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -31,8 +31,6 @@ import IdentitiesVotedTab from "./profile/IdentitiesVotedTab"
 import ProfileLayout from "./profile/ProfileLayout"
 
 import "../styles/global.css"
-import { ThemeProvider } from "./ThemeProvider"
-import VideoBanner from "./HomeBanner/VideoBanner"
 
 const API_URL = "https://prod.base.intuition-api.com/v1/graphql"
 configureClient({ apiUrl: API_URL })
