ee3a1c2938324bd682b0aab9adefb550037e90dc
RAMASSAMY Luc <luko974@gmail.com>
2025-05-26 14:33:33 +0200

    use another query in MyPositionsTab

--- src/components/profile/MyPositionsTab.tsx
+++ src/components/profile/MyPositionsTab.tsx
@@ -1,22 +1,30 @@
+import {
+  useGetClaimsByAddressQuery,
+  useGetTriplesByCreatorQuery,
+  useGetTriplesWithPositionsQuery
+} from "@warzieram/graphql"
 import React from "react"
+
 import { useStorage } from "@plasmohq/storage/hook"
+
 import ClaimRowLite from "../ui/ClaimRowLite"
-import { useGetClaimsByAddressQuery } from "@warzieram/graphql"
 
 const MyPositionsTab = () => {
   const [account] = useStorage<string>("metamask-account")
 
-  const { data, loading, error } = useGetClaimsByAddressQuery(
-    {variables: { address: account ?? "" }},
+  const { data, loading, error } = useGetTriplesWithPositionsQuery({
+    variables: { address: account ?? "" }
+  })
+
+  const claims = data?.triples ?? []
+  const filteredClaims = claims.filter(
+    (c) =>
+      Number(c.term?.positions_aggregate.aggregate?.count) > 0 ||
+      Number(c.counter_term?.positions_aggregate.aggregate?.count) > 0
   )
 
-  const claims = data?.claims_aggregate?.nodes ?? []
-  const filteredClaims = claims.filter(
-    (c) => Number(c.position.shares) > 0 || Number(c.counter_shares) > 0 // TODO: fix this (change the request ?)
-   )
-
   if (!account) return <div>No connected wallet</div>
-  if ( loading) return <div>Loading your positions...</div>
+  if (loading) return <div>Loading your positions...</div>
   if (error) return <div>Error: {(error as any)?.message}</div>
   if (filteredClaims.length === 0) return <div>No positions found</div>
 
@@ -30,23 +38,18 @@ const MyPositionsTab = () => {
       </p>
 
       {filteredClaims.map((claim, index) => {
-
-        console.log("CLAIM DEBUG", {
+        /* console.log("CLAIM DEBUG", {
           shares: claim.shares,
           counter_shares: claim.counter_shares,
           userStake: Number(claim.shares ?? 0),
           userCounterStake: Number(claim.counter_shares ?? 0)
-        })
+        })*/
 
-        const triple = claim.triple
-        
-        
-          return (
-            <ClaimRowLite
-              key={`${triple.id}-${index}`}
-              claim={triple}
-            />  
-          )
+        const triple = claim
+
+        return (
+          <ClaimRowLite key={`${triple.term_id}-${index}`} claim={triple} />
+        )
       })}
     </div>
   )
